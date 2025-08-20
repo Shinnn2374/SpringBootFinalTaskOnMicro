4c9f218e4e0960100a738c53fd916e94c4a03941
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-08-20 16:39:51 +0300

    ...

--- user-service/src/main/java/com/example/user_service/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.dao;
-
-import com.example.user_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
